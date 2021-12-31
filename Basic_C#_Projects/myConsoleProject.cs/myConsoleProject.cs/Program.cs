c3646ba7d35d8a6c7a7a879f99a30bcbc43b8af4
Matt <87787075+maty714@users.noreply.github.com>
2021-12-31 13:21:20 -0500

    Edited File

--- Basic_C#_Projects/myConsoleProject.cs/myConsoleProject.cs/Program.cs
+++ Basic_C#_Projects/myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace myConsoleProject.cs
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("My name is " + name);
+            Console.Read();
         }
     }
 }
