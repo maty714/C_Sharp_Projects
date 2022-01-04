da4d55917f52fa627731cd2b22563dc5295c3abc
Matt <87787075+maty714@users.noreply.github.com>
2022-01-04 00:21:55 -0500

    Daily Report
    Added a daily report project

---
+++ Basic_C#_Projects/Daily_Submission/Daily_Report/Daily_Report/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Report
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Title
+            Console.WriteLine("------------------");
+            Console.WriteLine("-The Tech Academy-");
+            Console.WriteLine("------------------");
+            Console.WriteLine("");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("____________________");
+
+
+            //Questions to be answered by students
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            string page = Console.ReadLine();
+
+            Console.Read();
+
+        }
+    }
+}
