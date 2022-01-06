882589e2c249b8e05c2c239ff07ab4517cd5a006
Matt <87787075+maty714@users.noreply.github.com>
2022-01-05 21:18:05 -0500

    Created Insurance Application Program

---
+++ Basic_C#_Projects/Insurance Application/Insurance_Application/Insurance_Application/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insurance_Application
+{
+    class Program
+    {
+        static void Main()
+        {
+            //prompts the user to answer questions
+            Console.WriteLine("Insurance Application");
+            Console.WriteLine("---------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Please input your age: ");
+            String input1 = Console.ReadLine();
+            int age = Convert.ToInt32(input1);
+
+            Console.WriteLine("Have you ever had a DUI?: ");
+            string input2 = Console.ReadLine();
+            bool DUI = Convert.ToBoolean(input2);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string input3 = Console.ReadLine();
+            int tickets = Convert.ToInt32(input3);
+
+            //output from user input
+
+            bool qualified = (age > 15 && DUI == false && tickets <= 3);
+
+           if (qualified == true)
+            {
+                Console.WriteLine("Qualified?: Yes");
+            }
+            else
+            {
+                Console.WriteLine("Qualified?: No");
+            }
+
+            Console.Read();
+        }
+    }
+}
