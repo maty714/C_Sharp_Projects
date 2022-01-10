7d30559a0d48600d8a2169eb76581170d32cf6a2
Matt <87787075+maty714@users.noreply.github.com>
2022-01-10 17:52:04 -0500

    Created a For_loop Folder and Project

---
+++ Basic_C#_Projects/For_Loop/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] myarray = {"1.", "2.", "3."};
+
+
+
+            //for (int i = 0; i < myarray.Length; i++)
+            //{
+            //    Console.WriteLine("Please enter some text that will be added to each element: ");
+            //    string input = Console.ReadLine();
+            //    myarray[i] += "" + input;
+
+            //}
+
+            //for (int i = 0; i < myarray.Length; i++)
+            //{
+            //    Console.WriteLine(myarray[i]);
+            //}
+
+
+            Console.WriteLine("Please enter some text that will be added to each element: ");
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < myarray.Length; i++)
+            {
+                Console.WriteLine(myarray[i] + " " + input);
+            }
+
+
+            Console.Read();
+
+
+        }
+    }
+}
