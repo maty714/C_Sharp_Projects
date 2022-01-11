94dfb2d862f5456dfee2a1552e8be9d131d3bda8
Matt <87787075+maty714@users.noreply.github.com>
2022-01-10 21:08:41 -0500

    Updated For_Loop App

--- Basic_C#_Projects/For_Loop/ConsoleApp1/ConsoleApp1/Program.cs
+++ Basic_C#_Projects/For_Loop/ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main()
         {
             string[] myarray = {"1.", "2.", "3."};
-
+            List<string> myList = new List<string>() { "car", "motorcycle", "truck", "airplane", "tractor", "golfcart" };
 
 
             //for (int i = 0; i < myarray.Length; i++)
@@ -28,18 +28,58 @@ namespace ConsoleApp1
             //}
 
 
-            Console.WriteLine("Please enter some text that will be added to each element: ");
-            string input = Console.ReadLine();
+            //Console.WriteLine("Please enter some text that will be added to each element: ");
+            //string input = Console.ReadLine();
 
-            for (int i = 0; i < myarray.Length; i++)
-            {
-                Console.WriteLine(myarray[i] + " " + input);
-            }
+            ////for loop using "<" operator
+            //for (int i = 0; i < myarray.Length; i++)
+            //{
+            //    Console.WriteLine(myarray[i] + " " + input);
+            //}
+            
+            ////infinite loop
+            //do
+            //{
+            //    Console.WriteLine("Infinite Loooop!");
+            //} while (false); //switch to true to create an infinite loop
+
+            ////for loop using "<=" operator
+            //for (int i = 0; i <= myarray.Length - 1; i++)
+            //{
+            //    Console.WriteLine(myarray[i] + " " + input);
+            //}
+
+            Console.WriteLine("Select an item from the list: ");
+            string item = Console.ReadLine();
+
+                for (int i = 0; i < myList.Count; i++)
+                {
+                    //if (item == "") 
+                    //{
+                    //    Console.WriteLine("Please search an item");
+                    //    break;
+                    //}
 
 
+                    while (item == "")
+                    {
+                    Console.WriteLine("Please enter a value: ");
+                    string input = Console.ReadLine();
+                    item = input;
+                    }
+
+                    if (item == myList[i])
+                    {
+                        Console.WriteLine("This item is in the list!");
+                        break;
+                    }
+                    else if (i == myList.Count - 1)
+                    {
+                        Console.WriteLine("This item does not exist in the list");
+                    }
+                }               
+            
             Console.Read();
-
-
         }
     }
 }
