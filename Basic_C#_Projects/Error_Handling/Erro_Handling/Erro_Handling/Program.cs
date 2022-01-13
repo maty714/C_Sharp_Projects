6f223990c91185791f4bca615b913e19a033f9c8
Matt <87787075+maty714@users.noreply.github.com>
2022-01-12 22:19:03 -0500

    Updated Error_Handling
    Added a simple app to prompt user input, then take that input and divide each item in a list from user prompt. Added a try catch in the mix to catch any inputs that are 0 or non integer.

---
+++ Basic_C#_Projects/Error_Handling/Erro_Handling/Erro_Handling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erro_Handling
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> myList = new List<int>() { 1,2,3,4,5,6,7 };
+
+            try { 
+            Console.WriteLine("Please enter a divisor: ");
+            string input = Console.ReadLine();
+            int num = Convert.ToInt32(input);
+
+
+            for (int i = 0; i < myList.Count; i++) 
+            {
+                Console.WriteLine("index " + i + " divided by " + num + " = " + myList[i] / num);
+            }
+            }
+
+            catch (DivideByZeroException dbz) //if user enters 0, displays message that user cannot divide by 0
+            {
+                Console.WriteLine(dbz.Message);
+                Console.WriteLine("Cannot divide by 0");
+            }
+
+            catch(FormatException fe)
+            {
+                Console.WriteLine(fe.Message);
+                Console.WriteLine("Please enter a number");
+            }
+
+            finally{
+                Console.Read();
+            }
+            
+        }
+    }
+}
