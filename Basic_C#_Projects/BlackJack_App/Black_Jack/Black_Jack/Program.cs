43ba63b73372f7f7e4ec2c3d5e0673df2ab8b565
Matt <87787075+maty714@users.noreply.github.com>
2022-01-16 22:38:22 -0500

    Added Functionality to Shuffle Method
    Shuffle Method can now take in how many times the deck will be shuffled.

--- Basic_C#_Projects/BlackJack_App/Black_Jack/Black_Jack/Program.cs
+++ Basic_C#_Projects/BlackJack_App/Black_Jack/Black_Jack/Program.cs
@@ -11,7 +11,8 @@ namespace Black_Jack
         static void Main(string[] args)
         {
             Deck deck = new Deck(); //this object automatically calls the constructor which values were set by the constructor. Since these are the current values of the class Deck properties, this is what will be displayed when looping through each "object" in the list
-            deck = shuffle(deck);
+            int timesShuffled = 0;
+            deck = shuffle(deck, out timesShuffled, 3);
 
             for (int i = 0; i < deck.Cards.Count; i++)// deck.cards comes from object created above
             {
@@ -19,22 +20,32 @@ namespace Black_Jack
                 Console.WriteLine(card.Face + " of " + card.Suit); // this writes out each property of each object above
             }
             Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Number of times shuffled: " + timesShuffled);
             Console.Read();
         }
 
-       public static Deck shuffle(Deck deck) //takes instance of class Deck as arg
+       public static Deck shuffle(Deck deck, out int timesShuffled, int times = 1) //takes instance of class Deck as arg
         {
-            List<Card> Templist = new List<Card>(); //Creates a temp list to store shuffled deck into
-            Random random = new Random(); //this is creating a new instance of the random class which is built in
+            
+            timesShuffled = 0;
+            for(int i = 0; i < times; i++) {
 
-            while (deck.Cards.Count > 0) //"deck" comes from the arg. because this is type class, we are able to access all the parameters
-            {
-                int randomIndex = random.Next(0, deck.Cards.Count);
-                Templist.Add(deck.Cards[randomIndex]);
-                deck.Cards.RemoveAt(randomIndex); //this removes each card from the deck and adds to the templist. this is also the counter for the loops condition. when a card is removed, the count goes down by 1 untill it is 0
+                timesShuffled++;
+                List<Card> Templist = new List<Card>(); //Creates a temp list to store shuffled deck into
+                Random random = new Random(); //this is creating a new instance of the random class which is built in
+                
+                
+                while (deck.Cards.Count > 0) //"deck" comes from the arg. because this is type class, we are able to access all the parameters
+                {
+
+                    int randomIndex = random.Next(0, deck.Cards.Count);
+                    Templist.Add(deck.Cards[randomIndex]);
+                    deck.Cards.RemoveAt(randomIndex); //this removes each card from the deck and adds to the templist. this is also the counter for the loops condition. when a card is removed, the count goes down by 1 untill it is 0
+                }
+                deck.Cards = Templist;  //remember deck.Cards, is the instance created above and it is accessing the list property that gets filled up through the constructor
             }
-            deck.Cards = Templist;  //remember deck.Cards, is the instance created above and it is accessing the list property that gets filled up through the constructor
-            return deck;
+
+            return deck; //this is returning the entire object with the new shuffled deck stored in templist which is equal to deck.cards
         }
     }
 }
