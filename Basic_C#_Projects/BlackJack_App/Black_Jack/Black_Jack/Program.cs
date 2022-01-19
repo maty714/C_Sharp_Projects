39fb9ea8d18546fd4cbf443665f8cbc022badb64
Matt <87787075+maty714@users.noreply.github.com>
2022-01-18 23:13:04 -0500

    Added Two Classes
    Added a Game class and a TwentyOneGame class which inherits from class Game.

--- Basic_C#_Projects/BlackJack_App/Black_Jack/Black_Jack/Program.cs
+++ Basic_C#_Projects/BlackJack_App/Black_Jack/Black_Jack/Program.cs
@@ -10,15 +10,20 @@ namespace Black_Jack
     {
         static void Main(string[] args)
         {
+            
             Deck deck = new Deck(); //this object automatically calls the constructor which values were set by the constructor. Since these are the current values of the class Deck properties, this is what will be displayed when looping through each "object" in the list
             int timesShuffled = 0;
             deck.shuffle(out timesShuffled, 3); //this will take the deck which was already created by the instructor and shuffle it. this part is done in the class Deck
 
+            
+            
             for (int i = 0; i < deck.Cards.Count; i++)// deck.cards comes from object created above
             {
                 Card card = deck.Cards[i]; // "Card card", in this situation, we are looping through an objects, so the variable "card" needs the type of "object". just like if we were looping through ints, that would then be an int relates to below, "deck.Cards[i] references the blank list that was filled up in the class Deck constructor
                 Console.WriteLine(card.Face + " of " + card.Suit); // this writes out each property of each object above
             }
+            
+            
             Console.WriteLine(deck.Cards.Count);
             Console.WriteLine("Number of times shuffled: " + timesShuffled);
             Console.Read();
