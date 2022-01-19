88faef73e9e16cc898e7c151a19738db8c9aa5b7
Matt <87787075+maty714@users.noreply.github.com>
2022-01-18 23:12:33 -0500

    Inheritance App
    Created an assignment that used inheritane

---
+++ Basic_C#_Projects/Classes/Method_Object/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee name = new Employee { FirstName = "Sample", LastName = "Student" };
+            name.SayName();
+            Console.Read();
+
+        }
+    }
+}
