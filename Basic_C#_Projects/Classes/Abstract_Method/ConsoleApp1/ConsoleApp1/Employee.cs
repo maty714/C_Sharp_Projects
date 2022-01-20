7c8cc686fefdeb2748c8fad3d5358f22af2533fa
Matt <87787075+maty714@users.noreply.github.com>
2022-01-19 22:43:27 -0500

    Abstract Method Creation
    Created an abstract class with an abstract method for an assignment.

---
+++ Basic_C#_Projects/Classes/Abstract_Method/ConsoleApp1/ConsoleApp1/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Employee: Person
+    {
+        public override void SayName() // this is needed in this class if you are to inherit from class Person
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+    }
+}
---
+++ Basic_C#_Projects/Classes/Abstract_Method/ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public abstract void SayName(); //what is the point of these? 
+        
+    }
+}
