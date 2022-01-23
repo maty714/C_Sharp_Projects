67f492c8bb12a62ec782abcd86df4d85fd49ae0d
Matt <87787075+maty714@users.noreply.github.com>
2022-01-23 14:52:26 -0500

    Created Overloading App
    Practiced using == overloading for an assignment

---
+++ Basic_C#_Projects/Classes/Operator_Overloading/Overloading/Overloading/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    public class Employee
+    {
+        //public Employee()
+        //{
+        //    this.ID = 0;
+
+        //}
+
+        //public Employee(int id)
+        //{
+        //    this.ID = id;
+
+        //}
+
+        public static bool operator ==(Employee Id1, Employee Id2) //these will not have arguments like traditional functions. they are what is being compared.
+        {
+            return (Id1.ID == Id2.ID);
+        }
+
+        public static bool operator !=(Employee Id1, Employee Id2)
+        {
+            return (Id1.ID != Id2.ID);
+        }
+
+        public int ID { get; set; }
+        
+    }
+}
---
+++ Basic_C#_Projects/Classes/Operator_Overloading/Overloading/Overloading/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee Id = new Employee();
+            Id.ID = 123;
+            Employee Id2 = new Employee();
+            Id2.ID = 123;
+
+            Console.WriteLine(Id == Id2);
+            Console.Read();
+        }
+    }
+}
