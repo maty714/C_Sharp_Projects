f06f62deca31fd4b1b38dfa360816e79a7dfb24a
Matt <87787075+maty714@users.noreply.github.com>
2022-01-24 19:59:59 -0500

    Updated Enum Assignment

--- Basic_C#_Projects/Classes/Enum Assignment/Enum_Assignment/Enum_Assignment/Program.cs
+++ Basic_C#_Projects/Classes/Enum Assignment/Enum_Assignment/Enum_Assignment/Program.cs
@@ -16,38 +16,39 @@ namespace Enum_Assignment
 
                 Day day = (Day)Enum.Parse(typeof(Day), dayOfWeek);
 
-                if (day == Day.monday)
-                {
-                    Console.WriteLine(Day.monday);
-                }
-                else if(day == Day.tuesday)
-                {
-                    Console.WriteLine(Day.tuesday);
-                }
-                else if (day == Day.wednesday)
-                {
-                    Console.WriteLine(Day.wednesday);
-                }
-                else if (day == Day.thursday)
-                {
-                    Console.WriteLine(Day.thursday);
-                }
-                else if (day == Day.friday)
-                {
-                    Console.WriteLine(Day.friday);
-                }
-                else if (day == Day.saturday)
-                {
-                    Console.WriteLine(Day.saturday);
-                }
-                else if (day == Day.sunday)
-                {
-                    Console.WriteLine(Day.sunday);
-                }
-                else
-                {
-                    Console.WriteLine("That day of the week does not exist");
-                }
+                Console.WriteLine(day);
+                //if (day == Day.monday)
+                //{
+                //    Console.WriteLine(Day.monday);
+                //}
+                //else if(day == Day.tuesday)
+                //{
+                //    Console.WriteLine(Day.tuesday);
+                //}
+                //else if (day == Day.wednesday)
+                //{
+                //    Console.WriteLine(Day.wednesday);
+                //}
+                //else if (day == Day.thursday)
+                //{
+                //    Console.WriteLine(Day.thursday);
+                //}
+                //else if (day == Day.friday)
+                //{
+                //    Console.WriteLine(Day.friday);
+                //}
+                //else if (day == Day.saturday)
+                //{
+                //    Console.WriteLine(Day.saturday);
+                //}
+                //else if (day == Day.sunday)
+                //{
+                //    Console.WriteLine(Day.sunday);
+                //}
+                //else
+                //{
+                //    Console.WriteLine("That day of the week does not exist");
+                //}
 
             }
             catch
