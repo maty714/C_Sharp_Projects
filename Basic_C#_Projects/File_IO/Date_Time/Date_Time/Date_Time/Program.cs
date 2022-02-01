1c940b18e1fee9d35294fd327c96a27f9982762c
Matt <87787075+maty714@users.noreply.github.com>
2022-01-31 22:04:06 -0500

    Created a DateTime App
    Simply DateTime app to practice using this struct with methods

---
+++ Basic_C#_Projects/File_IO/Date_Time/Date_Time/Date_Time/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            DateTime time_now = DateTime.Now; 
+            DateTime time = DateTime.Now.AddHours(num);
+
+            Console.WriteLine("The current time is {0}", time_now);
+            Console.WriteLine("The current time in 2 hours is {0}", time);
+
+            Console.Read();
+        }
+    }
+}
