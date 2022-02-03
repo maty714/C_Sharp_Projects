6ef79d6cbf2a5643191d1c2cb227a3f1f0a33acb
Matt <87787075+maty714@users.noreply.github.com>
2022-02-02 21:43:33 -0500

    Constructor_Assignment
    Small app which uses constructor chaining

---
+++ Basic_C#_Projects/Constructor/Constructor_Assignment/Constructor_Assignment/Constructor_Chain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Assignment
+{
+    class Constructor_Chain
+    {
+        public Constructor_Chain() : this(10)
+        {
+            // No parameter constructor method.// First Constructor  
+        }
+
+        public Constructor_Chain(int Age)
+        {
+            Console.WriteLine("This number is from the constructor chain " + Age); 
+        }
+
+    }
+
+}
---
+++ Basic_C#_Projects/Constructor/Constructor_Assignment/Constructor_Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Matt";
+            var age = 2;
+
+            Constructor_Chain constructor = new Constructor_Chain();
+
+
+            Console.WriteLine(name + " " + age);
+            Console.Read();
+        }
+    }
+}
