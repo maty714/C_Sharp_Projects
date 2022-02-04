f7ac7c18dd47f1ff524051b43efa7effcbb9c443
Matt <87787075+maty714@users.noreply.github.com>
2022-02-04 14:06:58 -0500

    Updated Black Jack App
    Added comments in the Fraud Exception class

--- Basic_C#_Projects/BlackJack_App/Black_Jack/Casino2/FraudException.cs
+++ Basic_C#_Projects/BlackJack_App/Black_Jack/Casino2/FraudException.cs
@@ -8,8 +8,8 @@ namespace Casino2
 {
    public  class FraudException: Exception
     {
-        public FraudException(): base() { }
-        public FraudException(string message) : base(message) { }
+        public FraudException(): base() { } //we utitilize this contructor when we DO NOT pass in any argument into the FraudException in class TwentyOneGame
+        public FraudException(string message) : base(message) { } //we utilize this contructor when passing an argument into the Fraud Exception in class TwentyOneGame
 
     }
 }
