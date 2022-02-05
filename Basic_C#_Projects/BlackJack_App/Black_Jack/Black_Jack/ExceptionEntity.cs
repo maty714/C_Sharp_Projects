56506b1176628c85a0038618efed2785880f03d4
Matt <87787075+maty714@users.noreply.github.com>
2022-02-05 15:47:15 -0500

    Create ExceptionEntity.cs
    Added a class to list out exceptions which will be printed to the console if asked.

---
+++ Basic_C#_Projects/BlackJack_App/Black_Jack/Black_Jack/ExceptionEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Black_Jack
+{
+    public class ExceptionEntity //"Entity is used to refer to a database object
+    {
+        public int Id { get; set; }
+        public string ExceptionType { get; set; }
+        public string ExceptionMessage { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
