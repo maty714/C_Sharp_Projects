ae23462bf82b1a4b3324044f7e272ef625ff6022
Matt <87787075+maty714@users.noreply.github.com>
2022-03-03 21:24:05 -0500

    Updated Banking App
    Updated the Database tables, added logic into the CreateAnAccount function.

--- Basic_C#_Projects/Banking/BankingApp/BankingApp/Controllers/HomeController.cs
+++ Basic_C#_Projects/Banking/BankingApp/BankingApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BankingApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,16 +14,38 @@ namespace BankingApp.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Login()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult CreateAnAccount(string firstName, string lastName, string emailAddress, string password)
         {
-            ViewBag.Message = "Your contact page.";
+
+            //if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress) || string.IsNullOrEmpty(password))
+            //{
+            //    return View("~/Views/Shared/Error.cshtml");
+            //}
+            //else
+
+
+
+            using (BankDataEntities db = new BankDataEntities())
+            {
+                var customer = new Customer_Info();
+                customer.FirstName = firstName;
+                customer.LastName = lastName;
+                customer.EmailAddress = emailAddress;
+                customer.Password = password;
+
+                db.Customer_Info.Add(customer); //this is where eveything we input is added, from here if you go to adminController, you will see how we set a variable to db.SignUps. This in turn will allow us to view the data through the admin view
+                db.SaveChanges(); //nothing will be saved to the database until we use this
+            }
+
+
+
 
             return View();
         }
