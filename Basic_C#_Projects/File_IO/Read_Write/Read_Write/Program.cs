c73b1d6f5c56e67f84df6dc0d2a5d3e5702413fa
Matt <87787075+maty714@users.noreply.github.com>
2022-05-09 23:23:16 -0400

    Update

---
+++ Basic_C#_Projects/File_IO/Read_Write/Read_Write/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Read_Write
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name in order for the program to read it back to you: ");
+            string name = Console.ReadLine();
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Matt\OneDrive\Documents\GitHub\C_Sharp_Projects\Basic_C#_Projects\File_IO\Read_Write\Name.txt"))
+            {
+                file.WriteLine(name);
+            }
+
+            string text = File.ReadAllText(@"C:\Users\Matt\OneDrive\Documents\GitHub\C_Sharp_Projects\Basic_C#_Projects\File_IO\Read_Write\Name.txt");
+            Console.WriteLine(text);
+            Console.Read();
+        }
+    }
+}
