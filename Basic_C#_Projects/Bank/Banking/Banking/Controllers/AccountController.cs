ec09e544c9423887a6d3c87276d771c4c5f46621
Matt <87787075+maty714@users.noreply.github.com>
2022-05-11 23:31:07 -0400

    Added parameter to registration model

--- Basic_C#_Projects/Bank/Banking/Banking/Controllers/AccountController.cs
+++ Basic_C#_Projects/Bank/Banking/Banking/Controllers/AccountController.cs
@@ -160,7 +160,7 @@ namespace Banking.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName }; 
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName, AccountSelection = model.AccountSelection }; 
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
